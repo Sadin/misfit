3f07df09a497ba8258af5cd3ced84c30670fe28b
Zachary Snyder <sadin@fedoraproject.org>
2018-05-06 12:00:07 -0400

    basis of logging fucntion started

--- app.py
+++ app.py
@@ -13,9 +13,7 @@ async def on_ready():
 |_|_|_|_|___|_| |_|_| 
     
     """)
-    print('Logged in as')
-    print(client.user.name)
-    print(client.user.id)
+    print(f'Login successful...\nusername:{client.user.name} | id:{client.user.id}')
     await client.change_presence(game=discord.Game(name='with unwanted toys'))
 
 # welcomes new user
@@ -28,42 +26,44 @@ async def on_member_join(member):
 @client.event
 async def on_message(message):
     # Establish Logging channel
-    channel = 413799927622139937
-    print(channel)
+    if not message.channel == client.get_channel('413799927622139937'):
+        await client.send_message(client.get_channel('413799927622139937'), str(message.content))
 
 
-    # Bot only logs chats it has perms to read.
-    # Prints output of chat to console botis running in.
-    print('{}, sent by: {}, @ {}, in {} | #{}\n'.format(message.content, message.author, message.timestamp, message.server, message.channel))
-    print(message.type)
-    # Duplicated all messages to the #Logs Channel
+        # Bot only logs chats it has perms to read.
+        # Prints output of chat to console botis running in.
+        print('{}, sent by: {}, @ {}, in {} | #{}\n'.format(message.content, message.author, message.timestamp, message.server, message.channel))
+        print(message.type)
+        # Duplicated all messages to the #Logs Channel
 
 
-    #Help menu for those new to the bot.abs
-    if message.content.startswith('!help'):
-        await client.send_message(message.author, 'https://github.com/Sadin/misfit/wiki/Help')
-        await client.send_message(message.channel, 'I sent you a DM.')
+        #Help menu for those new to the bot.abs
+        if message.content.startswith('!help'):
+            await client.send_message(message.author, 'https://github.com/Sadin/misfit/wiki/Help')
+            await client.send_message(message.channel, 'I sent you a DM.')
 
-    # Simple Ping Command.
-    if message.content.startswith('!ping'):
-       await client.send_message(message.channel, 'Pong!')
+        # Simple Ping Command.
+        if message.content.startswith('!ping'):
+            await client.send_message(message.channel, 'Pong!')
 
-    # looks for !test command
-    if message.content.startswith('!test'):
-        counter = 0
-        tmp = await client.send_message(message.channel, 'Calculating messages...')
-        async for log in client.logs_from(message.channel, limit=100):
-            if log.author == message.author:
-                counter += 1
-        await client.edit_message(tmp, 'You have sent {} messages when ive been online, @{}'.format(counter, message.author))
-        print(message.author, 'told me to count messages in', message.channel, 'chat, on ', message.server)
-    elif message.content.startswith('!sleep'):
-        await asyncio.sleep(5)
-        await client.send_message(message.channel, 'Done sleeping')
-        print(message.author, 'in', message.channel, 'chat, on ', message.server, ', told me to sleep')
+        # looks for !test command
+        if message.content.startswith('!test'):
+            counter = 0
+            tmp = await client.send_message(message.channel, 'Calculating messages...')
+            async for log in client.logs_from(message.channel, limit=100):
+                if log.author == message.author:
+                    counter += 1
+            await client.edit_message(tmp, 'You have sent {} messages when ive been online, @{}'.format(counter, message.author))
+            print(message.author, 'told me to count messages in', message.channel, 'chat, on ', message.server)
+        elif message.content.startswith('!sleep'):
+            await asyncio.sleep(5)
+            await client.send_message(message.channel, 'Done sleeping')
+            print(message.author, 'in', message.channel, 'chat, on ', message.server, ', told me to sleep')
 
-    if message.content.startswith('!announce'):
-        await asyncio.sleep(1)
+        if message.content.startswith('!announce'):
+            await asyncio.sleep(1)
+    else:
+        print(f'Will not log chats sent to #logs')
 
 async def on_error():
     print('Fatal Error')
